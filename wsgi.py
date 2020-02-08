b1f4daed88f1170a50b5b07c1d4be0885e777d3d
ghrevo <58859445+ghrevo@users.noreply.github.com>
2020-02-08 21:06:44 +0100

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -2,16 +2,69 @@ from flask import Flask
 from flask import request
 import json
 import os
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.webdriver import phantomjs
+from selenium.webdriver.common import utils
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from . import MyPhantomWebDriver
+#https://realpython.com/absolute-vs-relative-python-imports/
+#from . import MyPhantomWebDriver
 import platform
 import glob
 import socket
+class MyPhantomJSService(phantomjs.service.Service):
+    def __init__(self, executable_path, port=0, service_args=None, log_path=None, ip=None):
+        if ip is None:
+            self.ip = '0.0.0.0'
+        else:
+            self.ip = ip
+        phantomjs.service.Service.__init__(self, executable_path, port, service_args, log_path)
 
+    def command_line_args(self):
+        return self.service_args + ["--webdriver=%s:%d" % (self.ip, self.port)]
+
+    def is_connectable(self):
+        return utils.is_connectable(self.port, host=self.ip)
+
+    @property
+    def service_url(self):
+        return "http://%s:%d/wd/hub" % (self.ip, self.port)
+
+
+class MyPhantomWebDriver(RemoteWebDriver):
+
+    def __init__(self, executable_path="phantomjs",
+                 ip=None, port=0, desired_capabilities=DesiredCapabilities.PHANTOMJS,
+                 service_args=None, service_log_path=None):
+
+        self.service = MyPhantomJSService(
+            executable_path,
+            port=port,
+            service_args=service_args,
+            log_path=service_log_path,
+            ip=ip)
+        self.service.start()
+
+        try:
+            RemoteWebDriver.__init__(
+                self,
+                command_executor=self.service.service_url,
+                desired_capabilities=desired_capabilities)
+        except Exception:
+            self.quit()
+            raise
+
+        self._is_remote = False
+
+    def quit(self):
+
+        try:
+            RemoteWebDriver.quit(self)
+        except Exception:
+            # We don't care about the message because something probably has gone wrong
+            pass
+        finally:
+            self.service.stop()
+			
 application = Flask(__name__)
 
 @application.route("/", methods=['POST'])
