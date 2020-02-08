cafbc6dc3e7aed2533b7b274f3638e4fe12a1709
ghrevo <58859445+ghrevo@users.noreply.github.com>
2020-02-08 20:38:59 +0100

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from ./myphantomjs import MyPhantomWebDriver
+from .myphantomjs import MyPhantomWebDriver
 import platform
 import glob
 import socket
